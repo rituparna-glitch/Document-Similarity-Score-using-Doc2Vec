0fea4d0754e114894ce82add9646d9288f0810a6
rituparna-glitch <79700017+rituparna-glitch@users.noreply.github.com>
2022-06-17 13:25:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ def cleaned_doc(text):
     #remove words that are not alphabetic
     alpha_words = [word for word in word_tokens if word.isalpha()]
     #remove stopwords
-    cleaned_words = [word for word in alpha_words if not word in stopwords.words('english')]
-    return cleaned_words
+    #cleaned_words = [word for word in alpha_words if not word in stopwords.words('english')]
+    return alpha_words
 
 @app.route('/')
 def home():
@@ -33,8 +33,8 @@ def score():
     #for rendering result in html GUI
     text1 = request.form['document 1']
     text2 = request.form['document 2']
-    vec1 = model.infer_vector(text1)
-    vec2 = model.infer_vector(text2)
+    vec1 = model.infer_vector(cleaned_doc(text1))
+    vec2 = model.infer_vector(cleaned_doc(text2))
     cos_distance = spatial.distance.cosine(vec1, vec2)
     cosine_sim = 1 - cos_distance
 
